da0eac7503c4ea3e806eb7b3b86063bd997da1b9
stefanv <sjvdwalt@gmail.com>
2021-10-14 20:18:32 +0000

    Deploying to gh-pages from  @ 9f07e9cfbfab1a6399bb2e49723cc4a1aa2c110c 🚀

---
+++ component---src-pages-job-jsx-d0a80e8b61368e7d8a92.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"6UIS":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),o=n.n(a),r=n("Wbzz"),c=n("t78G"),u=n("+dvn");t.default=function(e){var t=e.pageContext.job;return o.a.createElement(c.a,null,o.a.createElement(u.a,{job:t}),"Return to ",o.a.createElement(r.a,{to:"/"},"jobs listing"),".")}}}]);
+//# sourceMappingURL=component---src-pages-job-jsx-d0a80e8b61368e7d8a92.js.map
